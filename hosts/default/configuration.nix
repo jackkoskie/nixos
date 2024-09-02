03eba6030fe5c5a445d12fe9d2935c05d325e997
jackkoskie <jack@koskie.ca>
2024-09-02 18:04:06 -0400

    add discord

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -98,8 +98,6 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    neovim
-    alacritty
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
